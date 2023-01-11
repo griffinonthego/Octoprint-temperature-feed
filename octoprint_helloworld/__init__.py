23e404c079f75edc6a8d4810cb4a9f199a517dc5
griffinonthego <griffinonthego@gmail.com>
2023-01-10 20:08:20 -0800

    Another commit

--- octoprint_helloworld/__init__.py
+++ octoprint_helloworld/__init__.py
@@ -1,9 +1,13 @@
 import octoprint.plugin
 
 class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
-                       octoprint.plugin.TemplatePlugin):
+                       octoprint.plugin.TemplatePlugin,
+                       octoprint.plugin.SettingsPlugin):
     def on_after_startup(self):
-        self._logger.info("Hello World!")
+        self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
+
+    def get_settings_defaults(self):
+        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
 
 __plugin_name__ = "Hello World"
 __plugin_pythoncompat__ = ">=3.7,<4"
